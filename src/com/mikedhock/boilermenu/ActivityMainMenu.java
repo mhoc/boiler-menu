182fa20ad5c6dcf291292e38da7f81a0f4794af4
Michael Hockerman <mike@hockerman.com>
2013-09-10 21:55:44 -0400

    I dont like the way the menu is currently designed, so Im throwing it all out and starting from scratch. It uses fragments, but doesnt USE fragments. We can do better. The backend is still good, though.

--- src/com/mikedhock/boilermenu/ActivityMainMenu.java
+++ src/com/mikedhock/boilermenu/ActivityMainMenu.java
@@ -15,29 +15,10 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 public class ActivityMainMenu extends Activity {
-
-	// Basically, which fragment is currently being displayed in the view. 
-	// This way we can track it for switching fragments and for the back button.
-	FragmentContext fragContext;
-	enum FragmentContext {
-		frag_location_list,
-		frag_meal_list
-	}
-	
-	// When the user makes choices in the menu structure, they will be stored here so 
-	// they can be accessed later when populating the menu list. I don't like this way of 
-	// doing it, but for now it will work.
-	public static Meal.Location locationSelected = null;
-	public static Meal.Time timeSelected = null;
 	
 	// Fragment manager to handle all the fragment transactions.
 	FragmentManager manager;
-	
-	// The bitmaps for all the menu items are pre-generated in onCreate.
-	// This way the app doesn't feel laggy when the user wants to switch views.
-	public static Bitmap[] locationBitmaps;
-	public static Bitmap[] mealBitmaps;
-	
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,19 +26,6 @@ public class ActivityMainMenu extends Activity {
         
         // Get the fragment manager which will be used to commit fragment transactions to the main view.
         manager = getFragmentManager();
-        
-        // Pre-generate all the bitmaps that we want to use for the menus
-        generateBitmaps();
-        
-        // We'll make the default fragment by-location, so set that up.
-        FragLocationList byLocationListFrag = new FragLocationList();
-        this.fragContext = FragmentContext.frag_location_list;
-        manager.beginTransaction().replace(R.id.main_content_frame, byLocationListFrag).commit();
-        
-        // Testing framework. Its pretty official.
-        // Comment out the test fragment you want to run, then comment the three lines above. 
-        // manager.beginTransaction().replace(R.id.main_content_frame, new DBTest()).commit();
-        // manager.beginTransaction().replace(R.id.main_content_frame, new WebSourceTest()).commit();
         
     }
 
@@ -71,44 +39,9 @@ public class ActivityMainMenu extends Activity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case R.id.menu_main_switch_views:
-			if (this.fragContext == FragmentContext.frag_location_list) {
-				FragTimeList byMealListFrag = new FragTimeList();
-				this.fragContext = FragmentContext.frag_meal_list;
-				manager.beginTransaction().replace(R.id.main_content_frame, byMealListFrag).commit();
-			} else if (this.fragContext == FragmentContext.frag_meal_list) {
-				FragLocationList byLocationListFrag = new FragLocationList();
-				this.fragContext = FragmentContext.frag_location_list;
-				manager.beginTransaction().replace(R.id.main_content_frame, byLocationListFrag).commit();
-			}
-			break;
-		}
 		
 		return true;
 	}
 	
-	/** Generates all the bitmaps which will be used in the app. 
-	 *  It is done here and stored as a static class variable in memory so that all the heavy lifting in
-	 *  the app is done while it is launching. */
-	private void generateBitmaps() {
-		Resources r = getResources();
-		
-		locationBitmaps = new Bitmap[5];
-		locationBitmaps[0] = BitmapFactory.decodeResource(r, R.drawable.loc_earhart_outside);
-		locationBitmaps[1] = BitmapFactory.decodeResource(r, R.drawable.loc_ford_outside);
-		locationBitmaps[2] = BitmapFactory.decodeResource(r, R.drawable.loc_hillenbrand_outside);
-		locationBitmaps[3] = BitmapFactory.decodeResource(r, R.drawable.loc_wiley_outside);
-		locationBitmaps[4] = BitmapFactory.decodeResource(r, R.drawable.loc_windsor_outside);
-		
-		mealBitmaps = new Bitmap[3];
-		mealBitmaps[0] = BitmapFactory.decodeResource(r, R.drawable.meal_breakfast);
-		mealBitmaps[1] = BitmapFactory.decodeResource(r, R.drawable.meal_lunch);
-		mealBitmaps[2] = BitmapFactory.decodeResource(r, R.drawable.meal_dinner);
-	}
-    
-    
-    
-    
-
+	
 }
