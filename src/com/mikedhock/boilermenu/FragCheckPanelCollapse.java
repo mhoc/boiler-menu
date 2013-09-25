7bf3f5a60b15ae2ec4f71546e8e08de030ed8597
Michael Hockerman <mike@hockerman.com>
2013-09-25 18:55:45 -0400

    And now the animation is working both ways. Awesome.

--- src/com/mikedhock/boilermenu/FragCheckPanelCollapse.java
+++ src/com/mikedhock/boilermenu/FragCheckPanelCollapse.java
@@ -1,15 +1,27 @@
 package com.mikedhock.boilermenu;
 
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 
-public class FragCheckPanelCollapse extends Fragment {
+public class FragCheckPanelCollapse extends Fragment implements OnClickListener {
 
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.frag_check_panel_collapsed, container, false);
+		View v = inflater.inflate(R.layout.frag_check_panel_collapsed, container, false);
+		v.setOnClickListener(this);
+		return v;
+	}
+
+	public void onClick(View arg0) {
+		FragmentTransaction transaction = this.getFragmentManager().beginTransaction();
+		transaction.setCustomAnimations(R.anim.frag_slide_down, android.R.animator.fade_out);
+		transaction.detach(this);
+		transaction.replace(R.id.main_checkpanel_expanded, new FragCheckPanelExpand());
+		transaction.commit();
 	}
 	
 	
